fed0fe1cb18a161118c2c8b75ec50b886eb622de
naza1 <naza82@gmail.com>
2019-08-28 23:21:17 -0300

    Fix go back button and refactor service worker

--- subir.php
+++ subir.php
@@ -1,71 +1,3 @@
-<!DOCTYPE html>
-
-<html lang="es">  
-<head>
-<meta charset="utf-8">
-  <title>AVA | Animales Varados y Atropellados</title>
-
-
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-<link rel="stylesheet" type="text/css" href="css/imgeffect.css" />
-<link rel="shortcut icon" href="favicon.ico">	
-
-  <meta name="apple-mobile-web-app-title" content="AVA | Animales Varados y Atropellados">
-
-  <link rel="apple-touch-startup-image" href="img/apple-touch-startup-image-640x1096.png"
-    media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)">
-  <!-- iPhone 5+ -->
-  <link rel="apple-touch-startup-image" href="img/apple-touch-startup-image-640x920.png"
-    media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2)">
-  <!-- iPhone, retina -->
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black">
-  <meta name="msapplication-TileColor" content="#54E5AA" />
-  <link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="img/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
-  <meta name="msapplication-TileColor" content="#54E5AA">
-  <meta name="msapplication-TileImage" content="img/apple-icon-144x144.png">
-  <meta name="theme-color" content="#54E5AA">
-  <meta name="HandheldFriendly" content="True">
-  <meta name="MobileOptimized" content="320">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-  <link rel="manifest" href="manifest.json">
-  
-</head> 
-<body>
-
-	<div class="header_bg">
-					<div class="wrap">
-						<div class="header">
-							<!--------logo------>
-							<div class="logo">
-								<a href="index.php"><img src="img/Logo-ava-01.png" alt="" /></a>
-							</div>	
-							<!--------finde-logo--------->
-							<!----start-nav-------->	
-							<div class="nav">
-								<div id="leyenda">
-									Animales Varados <br/>y Atropellados
-								</div>
-							</div>
-							<!-----end-nav-------->
-							<div class="clear"> </div>
-						</div>
-					</div>
-	</div>
-
- <div class="mensaje">
 <?php 
 
   require 'conectar.php';
@@ -86,7 +18,8 @@
         VALUES ('$longitud','$latitud','$email','$comentarios','$fecha', '$fileName')";
   if (mysqli_query($conn, $sql)) 
   {
-    echo "Su fotograf&iacute;a ha sido almacenada en nuestros registros.<br> Recibir&aacute; actualizaciones en el correo electr&oacute;nico registrado";
+    //echo "Su fotograf&iacute;a ha sido almacenada en nuestros registros.<br> Recibir&aacute; actualizaciones en el correo electr&oacute;nico registrado";
+    header('Location: subir.html');
   } 
   else 
   {
@@ -94,16 +27,3 @@
   }
   mysqli_close($conn);
 ?>
-
-</div>
-<a href="index.php" class="button2"> Subir otra fotograf&iacute;a</a>
- <!--footer--> 			  
-<div class="footer-bottom">
-     	<div class="wrap">
-        <div class="copy">
-		    <p class="copy">AVA: Animales Varados y Atropellados. <br/>Copyright 2019</p>
-		</div>
-	</div>
-</div>
-</body> 
-</html>
--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 // Asignar nombre y versión de caché
 const CACHE_NAME = 'v1_cache_ava';
+var VERSION = '20';
 
 //Ficheros de caché para ver la app offline, dejar todo en raiz, sino hay que especificas donde está la css, las imagenes, etc.
 var urlsToCache = ['./',];
@@ -40,18 +41,50 @@ self.addEventListener('activate', e => {
 });
 
 // Evento fetch , para actualizar la app
-self.addEventListener('fetch', e => {
-	e.respondWith(
-		caches.match(e.request)
-			.then(res => {
-				if(res){
-					//devuelve los datos de caché
-					return res; 
-				}
-				return fetch(e.request);
-			})
-	)
-});
+// self.addEventListener('fetch', e => {
+// 	e.respondWith(
+// 		caches.match(e.request)
+// 			.then(res => {
+// 				if(res){
+// 					//devuelve los datos de caché
+// 					return res; 
+// 				}
+// 				return fetch(e.request);
+// 			})
+// 	)
+// });
 
+this.addEventListener('fetch', function(e) {
+	var tryInCachesFirst = caches.open(VERSION).then(cache => {
+	  return cache.match(e.request).then(response => {
+		if (!response) {
+		  return handleNoCacheMatch(e);
+		}
+		// Update cache record in the background
+		fetchFromNetworkAndCache(e);
+		// Reply with stale data
+		return response
+	  });
+	});
+	e.respondWith(tryInCachesFirst);
+  });
 
+  function handleNoCacheMatch(e) {
+	return fetchFromNetworkAndCache(e);
+  }
+
+  function fetchFromNetworkAndCache(e) {
+	return fetch(e.request).then(res => {
+	  // foreign requests may be res.type === 'opaque' and missing a url
+	  if (!res.url) return res;
+	  // regardless, we don't want to cache other origin's assets
+	  if (new URL(res.url).origin !== location.origin) return res;
+  
+	  return caches.open(VERSION).then(cache => {
+		// TODO: figure out if the content is new and therefore the page needs a reload.
+		cache.put(e.request, res.clone());
+		return res;
+	  });
+	}).catch(err => console.error(e.request.url, err));
+  }
 
